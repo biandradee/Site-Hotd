6474b1d6f3f4737f0c61230b5871eeb382772f36
Bianca Andrade <emi.andrade37@hotmail.com>
2023-07-05 11:50:23 -0300

    Change BrowserRouter for HashRouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import React from 'react'
 import Home from './pages/home';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import Dragões from './pages/fotos2/fotos2';
 import Personagens from './pages/fotos/fotos';
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/contato' element={<Dragões />}/>
         <Route path='/fotos' element={<Personagens />}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
